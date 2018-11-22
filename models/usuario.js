987e8882a57815c73fda7b5052f0fc3668c8e797
Vale <torresvalemdz@gmail.com>
2018-11-22 16:55:29 -0300

    model en node

---
+++ models/usuario.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var usuarioSchema = new Schema({
+    nombre: { type: String, required: [true, "El nombre es requerido"] },
+    email: { type: String, unique: true, required: [true, "El email es requerido"] },
+    password: { type: String, required: [true, "La contraseña es requerida"] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE' }
+});
+
+module.exports.mongoose.model('Usuario', usuarioSchema);
